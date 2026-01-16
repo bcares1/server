c28c65a21583e08ab2e328fc78012b7e6db127d6
bcares1 <x5@eveire.com>
2026-01-16 23:16:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -747,8 +747,7 @@ io.on("connection", (socket) => {
 });
 
 // ─── 8) MONGODB CONNECT & SERVER START ────────────────────────────────────────
-mongoose
-  .connect('mongodb+srv://x4_db_user:ZY81zH9tPOyLhkNe@cluster0.ynqrnlv.mongodb.net/?appName=Cluster0', {
+mongoose.connect('mongodb+srv://a:a@cluster0.ynqrnlv.mongodb.net/?appName=Cluster0',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -761,5 +760,6 @@ mongoose
     console.error("❌ MongoDB connection error:", err);
     process.exit(1);
   });
+
 
 
